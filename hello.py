43721045310bafe87d630cdc2fe26bd17011897f
Manuel Jarry <lemanujarry@gmail.com>
2024-09-04 15:56:24 -0400

    Add hello

--- hello.py
+++ hello.py
@@ -1,5 +1,10 @@
 import math
 #TODO Affichez l'aire d'un cercle avec le rayon indiqué
 rayon = 4
+airCercle = math.pi * (rayon^2)
+print(airCercle)
+print("Manu")
+print("Demande jamais à un gars comme s'est passé sa journée")
+
 
 #TODO Ajoutez une ligne qui affichera votre nom et prénom à la fin
